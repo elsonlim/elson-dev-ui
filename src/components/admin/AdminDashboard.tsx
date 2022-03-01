03c6ff9d081284a0ead234f7927a161aa08a3d3b
Elson Lim <elsonlim7@gmail.com>
2022-03-01 22:45:18 +0800

    Move UserDataGrid out of AdminDashboard

--- src/components/admin/AdminDashboard.tsx
+++ src/components/admin/AdminDashboard.tsx
@@ -1,21 +1,21 @@
 import { FC, useState, useEffect } from "react";
-import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
-
-import { Edit as EditIcon } from "@mui/icons-material";
 import { httpClient } from "./axios";
 import { User as UserType } from "../navbar/userInterfaces";
 import EditUserModal from "./EditUserModal";
+import UserDataGrid from "./UserDataGrid";
 
 const AdminDashboard: FC = () => {
   const [user, setUser] = useState();
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedRow, setSelectedRow] = useState<UserType>();
   const [users, setUsers] = useState();
+
   useEffect(() => {
     httpClient.get("/user", { withCredentials: true }).then((res) => {
       console.log(res.data);
       setUser(res.data.name);
     });
+
     httpClient.get("/admin/users", { withCredentials: true }).then((res) => {
       const users = res.data.map((data: any) => {
         data.id = data._id;
@@ -26,43 +26,10 @@ const AdminDashboard: FC = () => {
     });
   }, []);
 
-  const columns = [
-    { field: "name", headerName: "Name", width: 150 },
-    {
-      field: "role",
-      headerName: "Role",
-      width: 150,
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      width: 300,
-    },
-    {
-      field: "approved",
-      headerName: "Approved",
-      width: 100,
-      type: "boolean",
-    },
-    { field: "apps", headerName: "Apps", width: 200 },
-    {
-      field: "actions",
-      headerName: "Actions",
-      width: 100,
-      renderCell: (props: GridRenderCellParams) => {
-        return (
-          <div
-            onClick={() => {
-              setModalOpen(true);
-              setSelectedRow(props.row);
-            }}
-          >
-            <EditIcon />
-          </div>
-        );
-      },
-    },
-  ];
+  const handleOpenModal = (user: UserType) => {
+    setModalOpen(true);
+    setSelectedRow(user);
+  };
 
   const handleCloseModal = () => {
     setModalOpen(false);
@@ -72,12 +39,7 @@ const AdminDashboard: FC = () => {
   return (
     <div style={{ height: "300px" }}>
       {users && (
-        <DataGrid
-          rows={users}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
-        />
+        <UserDataGrid users={users} handleOpenModal={handleOpenModal} />
       )}
 
       {selectedRow && (
---
+++ src/components/admin/UserDataGrid.tsx
@@ -0,0 +1,59 @@
+import { FC } from "react";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { Edit as EditIcon } from "@mui/icons-material";
+import { User as UserType } from "../navbar/userInterfaces";
+
+interface IUserDataGrid {
+  users: UserType[];
+  handleOpenModal: (user: UserType) => void;
+}
+
+const UserDataGrid: FC<IUserDataGrid> = ({ users, handleOpenModal }) => {
+  const columns = [
+    { field: "name", headerName: "Name", width: 150 },
+    {
+      field: "role",
+      headerName: "Role",
+      width: 150,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 300,
+    },
+    {
+      field: "approved",
+      headerName: "Approved",
+      width: 100,
+      type: "boolean",
+    },
+    { field: "apps", headerName: "Apps", width: 200 },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (props: GridRenderCellParams) => {
+        return (
+          <div
+            onClick={() => {
+              handleOpenModal(props.row);
+            }}
+          >
+            <EditIcon />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <DataGrid
+      rows={users}
+      columns={columns}
+      pageSize={5}
+      rowsPerPageOptions={[5]}
+    />
+  );
+};
+
+export default UserDataGrid;
